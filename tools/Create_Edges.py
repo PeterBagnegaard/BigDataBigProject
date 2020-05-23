c8760034fa4abed22015b3d8b10161c451685d60
RasmusOrsoe <48880272+RasmusOrsoe@users.noreply.github.com>
2020-05-23 16:28:23 +0200

    Add files via upload

---
+++ tools/Create_Edges.py
@@ -0,0 +1,17 @@
+def Create_Edges(data,event_no):
+    index = data['event_no'] == event_no
+    event = pd.DataFrame(data.loc[index,:])
+    upper = list([0])
+    lower = list([1])
+    for k in range(1,len(event)):
+        upper.append(k)
+        if k != (len(event) - 1):
+            upper.append(k)
+    for j in range(1,len(upper)-2,2):
+        lower.append(upper[j-1])
+        lower.append(upper[j+2])
+    lower.append(upper[len(upper) - 2])
+    
+    edge_index = torch.tensor([upper,
+                               lower], dtype = torch.long)
+    return edge_index
